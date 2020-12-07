3afe12c699a40d556342eac7db8bca253e7c4cfd
jboothby <64981697+jboothby@users.noreply.github.com>
2020-12-07 10:11:53 -0800

    Finished cumulativeSum() tests

--- testCorrectness.js
+++ testCorrectness.js
@@ -10,4 +10,13 @@ describe('primefunctions.js', function () {
       chai.expect(primeGen(20)).to.deep.equal([2, 3, 5, 7, 11, 13, 17, 19]);
     });
   });
+  // test cumulativeSum() method
+  describe('cumulativeSum()', function () {
+    it('cumulativeSum([1, 2, 3, 4])=>[1, 3, 6, 10]', function () {
+      chai.expect(cumulativeSum([1, 2, 3, 4])).to.deep.equal([1, 3, 6, 10]);
+    });
+    it('cumulativeSum([10, 11, 12, 13, 14])=>[10, 21, 33, 46, 60]', function () {
+      chai.expect(cumulativeSum([10, 11, 12, 13, 14])).to.deep.equal([10, 21, 33, 46, 60]);
+    });
+  });
 });
